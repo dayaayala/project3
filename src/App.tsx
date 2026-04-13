2d8fc535be99e0ea473129bd54b8d5c4395652cf
dayaayala <dayala2@vols.utk.edu>
2026-04-13 09:36:25 -0400

    oka

--- src/App.tsx
+++ src/App.tsx
@@ -394,7 +394,7 @@ function MapPage() {
           &lt;
         </Link>
       </div>
-      <MapContainer center={center} zoom={13} style={{ height: "100%", width: "100%" }}>
+      <MapContainer className="map-canvas" center={center} zoom={13} style={{ height: "100%", width: "100%" }}>
         <TileLayer
           attribution='&copy; OpenStreetMap contributors &copy; CARTO'
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
